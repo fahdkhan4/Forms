af844c868840bfbd03054f1f7a18f424da398a9f
Fahd <Fahdkhan487@gmail.com>
2022-06-09 11:56:12 -0700

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsComponent } from './components/reactive-forms/reactive-forms.component';
+import { TDFFormsComponent } from './components/tdf-forms/tdf-forms.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'TDF',component:TDFFormsComponent},
+  {path:'Reactive',component:ReactiveFormsComponent},
+  {path:'',component:TodoListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TDFFormsComponent } from './components/tdf-forms/tdf-forms.component';
+import { ReactiveFormsComponent } from './components/reactive-forms/reactive-forms.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TDFFormsComponent,
+    ReactiveFormsComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
